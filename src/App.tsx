42c69d30ab4df6dad3ee2e44ca9ee0a85841abb9
coldrainf <coldrainf@163.com>
2020-08-26 23:30:45 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,18 @@
 import React from 'react';
-import { Text } from 'react-native'
-import { Router, Stack, Scene, Tabs } from 'react-native-router-flux'
+import {
+  Text,
+} from 'react-native';
 
-import Home from './view/home'
-import All from './view/all'
-import Search from './view/search'
-const TabIcon = ({focused , title}) => {
-    return (
-      <Text style={{color: focused  ? 'blue' : 'black'}}>{title}</Text>
-    );
-  };
+import ScrollableTabView, { DefaultTabBar } from 'react-native-scrollable-tab-view';
+
 export default () => {
-    return(<Router>
-        {/*tabBarPosition设置tab是在top还是bottom */}
-        <Scene hideNavBar tabBarPosition="bottom">
-          <Tabs
-            key="tabbar"
-            swipeEnabled={true}
-            // wrap={false}
-            // 是否显示标签栏文字
-            showLabel={false}
-            tabBarStyle={{backgroundColor: "#eee"}}
-            //tab选中的颜色
-            activeBackgroundColor="white"
-            //tab没选中的颜色
-            inactiveBackgroundColor="black"
-          >
-            <Scene
-              key="one"
-              icon={TabIcon}
-              component={Home}
-              title="PageOne"
-            />
-     
-            <Scene
-              key="two"
-              component={All}
-              title="PageTwo"
-              icon={TabIcon}
-            />
-     
-            <Scene
-              key="three"
-              component={Search}
-              title="PageThree"
-              icon={TabIcon}
-            />
-          </Tabs>
-        </Scene>
-      </Router>)
+  return <ScrollableTabView
+    style={{ marginTop: 20 }}
+    initialPage={1}
+    renderTabBar={() => <DefaultTabBar />}
+  >
+    <Text tabLabel='Tab #1'>My</Text>
+    <Text tabLabel='Tab #2'>favorite</Text>
+    <Text tabLabel='Tab #3'>project</Text>
+  </ScrollableTabView>;
 }
