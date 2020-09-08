e46b9d3d619b45437cda93592142a4dd1ae413ca
coldrainf <coldrainf@163.com>
2020-09-08 23:27:32 +0800

    1

--- src/view/item.tsx
+++ src/view/item.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native'
 import { connect } from 'react-redux'
 import { Icon, Image } from 'react-native-elements'
 
@@ -7,6 +7,13 @@ import CustomButton from '../component/button'
 import Loading from '../component/loading'
 import api from '../../config/api'
 
+const chapterItem = (props: any) => {
+    return (
+        <View>
+            <Text>{props.item.name}</Text>
+        </View>
+    )
+}
 
 const Item = (props: BaseProps) => {
     const item = props.route.params
@@ -19,14 +26,16 @@ const Item = (props: BaseProps) => {
             setData(res.data)
         })
     }, [])
+
+    let [select, setSelect] = useState(0)
     return (
         <>
         {
             loading && <Loading />
         }
         {
-            !loading && <View style={{ backgroundColor: props.theme }}>
-                <View style={styles.headerContainer}>
+            !loading && <View>
+                <View style={[styles.headerContainer, { backgroundColor: props.theme }]}>
                     <Image
                         source={{ 
                             uri: data.cover,
@@ -42,12 +51,53 @@ const Item = (props: BaseProps) => {
                         <Text style={styles.detail}>{data.author?.join(' ')}</Text>
                         <Text style={styles.detail}>{data.type?.join(' ')}</Text>
                         <Text style={styles.detail}>{data.area}</Text>
-                        <Text style={styles.detail}>{data.desc}</Text>
+                        <Text style={styles.detail} numberOfLines={6}>{data.desc}</Text>
                     </View>
                 </View>
-                <View>
-                    
+                <View style={[styles.headerBottom, { backgroundColor: props.theme }]}>
+                    <Text style={styles.detail}>{data.status}</Text>
+                    <Text style={styles.detail}>{data.updateTime}</Text>
                 </View>
+                <View style={[styles.headerBottom, { backgroundColor: props.theme }]}>
+                    <View>
+                        {
+                            data?.chapters?.map((c: any, i: number) => (
+                                <View key={i}>
+                                    <Text>{c.title}</Text>
+                                </View>
+                            ))
+                        }
+                    </View>
+                    <View>
+                        <Text>正序</Text>
+                    </View>
+                </View>
+                {/* <FlatList
+                        refreshing={true}
+                        data={list}
+                        renderItem={itemProps => <Item {...itemProps} navigation={props.navigation} />}
+                        keyExtractor={(item, k) => k.toString()}
+                        horizontal={false}
+                        numColumns={3}
+                        showsVerticalScrollIndicator = {false}
+                        onEndReached={onEndReached}
+                        ListFooterComponent={<>
+                            {
+                                last && <View style={styles.footer}>
+                                    <Text>没有更多啦~</Text>
+                                </View>
+                            }
+                            {
+                                footerRefreshing && <View style={styles.footer}>
+                                    <ActivityIndicator
+                                        animating={true}
+                                        color={props.theme}
+                                        size="small"
+                                    />
+                                </View>
+                            }
+                        </>}
+                    /> */}
             </View>
         }
         </>
@@ -67,7 +117,7 @@ const styles = StyleSheet.create({
     headerContainer: {
         flexDirection: 'row',
         paddingHorizontal: 6,
-        paddingVertical: 12,
+        paddingTop: 8
     },
     imageContainer: {
         borderRadius: 4,
@@ -87,5 +137,16 @@ const styles = StyleSheet.create({
     detail: {
         color: '#fff',
         marginBottom: 4
+    },
+    headerBottom: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 6,
+        paddingVertical: 4
+    },
+    chapterHeaderContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 4
     }
 })
