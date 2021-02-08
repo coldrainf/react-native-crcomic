6197073740638f291432fda5d153132165d1a5d3
coldrainf <feilh@mail.jj.cn>
2021-02-08 14:18:25 +0800

    1

--- src/types/apiData.d.ts
+++ src/types/apiData.d.ts
@@ -2,62 +2,33 @@ interface Res {
     code: number,
     data: any
 }
-interface ItemBaseData {
-    id: string,
-    name: string,
-}
-
 interface FilterRes extends Res {
     data: FilterData
 }
-type FilterData = Array<Array<BaseData>>
 interface ListRes extends Res {
     data: ListData
 }
-type ListData = Array<BaseData>
-
-interface BaseData extends ItemBaseData {
-    default?: string,
-    cover?: string,
-    lastChapterId?: string,
-    lastChapterName?: string,
-    originId?: string,
-    originName?: string,
-    data?: Array<BaseData>
-}
-
+type ListData = Array<ItemBaseData>
 
 interface ItemRes extends Res {
     data: ItemData
 }
-interface ItemData extends ItemBaseData {
-    cover: string,
-    originId: string,
-    originName: string,
-    author: Array<string>
-    type: Array<string>
-    area: string
-    status: string,
-    updateTime?: string,
-    desc: string,
-    chapters: Array<ChapterData>
-}
 
 interface ChapterData {
     title: string,
-    data: Array<ItemBaseData>
+    data: Array<BaseData>
 }
 
 interface ImageRes extends Res {
     data: ImageData
 }
-interface ImageData extends ItemBaseData {
+interface ImageData extends BaseData {
     cover: string,
     originId: string,
     originName: string,
     chapterId: string,
     chapterName: string,
     images: Array<string>,
-    prev: ItemBaseData | null,
-    next: ItemBaseData | null,
+    prev: BaseData | null,
+    next: BaseData | null,
 }
